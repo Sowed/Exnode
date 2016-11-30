0ab7f6bf850b9c253254b2a7f166ffa221d8cd87
iamcastelli <sowed@cyberdude.com>
2016-11-30 19:38:26 +0300

    Test Pretify

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var express = require('express');
-var app= express();
+var app = express();
 
-app.get('/', function(request, response){
-	response.send('<h1>Hello</h1> Old Spot');
+app.get('/', function(request, response) {
+    response.send('<h1>Hello</h1> Old Spot');
 });
 
-app.get('/team', function(request, response){
-	response.send('<h1>Squad</h1> Oh, you wana meet the team!');
+app.get('/team', function(request, response) {
+    response.send('<h1>Squad</h1> Oh, you wana meet the team!');
 });
 
-app.get('/team/:name', function(request, response){
-	var name = request.params.name;
-	response.send('<h1>'+name+'</h1> Rocks this place.<br/>Twitter: @'+name);
+app.get('/team/:name', function(request, response) {
+    var name = request.params.name;
+    response.send('<h1>' + name + '</h1> Rocks this place.<br/>Twitter: @' + name);
 });
 
-app.get('/team/:name?/:skills?', function(request, response){
-	var name = request.params.name;
-	var skills = request.params.skills;
+app.get('/team/:name?/:skills?', function(request, response) {
+    var name = request.params.name;
+    var skills = request.params.skills;
 
-	response.send('<h1>'+name+'</h1> Rocks this place.<br/>Did you know, he is a Rockstar at: '+skills);
+    response.send('<h1>' + name + '</h1> Rocks this place.<br/>Did you know, he is a Rockstar at: ' + skills);
 });
 
-var server = app.listen(3030, function(){
-	console.log('Evesdropping on port 3030');
+var server = app.listen(3030, function() {
+    console.log('Evesdropping on port 3030');
 });
