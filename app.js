55bf002543c0322893514b15f747cf3f2c5ae340
iamcastelli <sowed@cyberdude.com>
2016-12-05 18:59:09 +0300

    Update app.js to support routing and Mongo, Add dependenices in package.json

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongo = require('mongodb');
+var monk = require('monk');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views-jade'));
+app.set('view engine', 'jade');
 
-app.get('/', function(request, response) {
-    response.render('layout',{'title':'Tutor'});
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Make our Mongo Database accesible to the app
+var db = monk('localhost:27017/exnodedb');
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
 });
 
+app.use('/', index);
+app.use('/users', users);
 
-/*
-app.get('/', function(request, response) {
-    response.send('<h1>Hello</h1> Old Spot');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    console.log(err);
+
+    //res.send('404: Page not Found', 404);
+    res.status(400);
+    res.render('error-404', {
+        title: '404: Page Not Found',
+        message: err.message,
+        error: err
+    });
+    //next(err);
 });
 
-app.get('/team', function(request, response) {
-    response.send('<h1>Squad</h1> Oh, you wana meet the team!');
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error-500', {
+            title: '500: Internal Server Error',
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.get('/team/:name', function(request, response) {
-    var name = request.params.name;
-    response.send('<h1>' + name + '</h1> Rocks this place.<br/>Twitter: @' + name);
-});
-
-app.get('/team/:name?/:skills?', function(request, response) {
-    var name = request.params.name;
-    var skills = request.params.skills;
-
-    response.send('<h1>' + name + '</h1> Rocks this place.<br/>Did you know, he is a Rockstar at: ' + skills);
-});
-
-app.get('*', function(request, response) {
-	response.send('<h1>Well, seem you are lost</h1><br/>Go back to <a href="/">Home</a>');
-});
-*/
-
-var server = app.listen(3030, function() {
-    console.log('Evesdropping on port 3030');
+var server = app.listen(3040, function() {
+    console.log('Evesdropping on port 3040');
 });
